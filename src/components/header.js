436fc0e954fda13a1619a9b454274293127be92e
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-12-10 13:37:48 -0600

    header link update

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Fragment } from 'react'
+import {Link} from 'gatsby'
 import { Popover, Transition } from '@headlessui/react'
 import {
     BookmarkAltIcon,
@@ -85,14 +86,14 @@ export default function Header() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6">
                 <div className="flex justify-between items-center py-6 md:justify-start md:space-x-10">
                     <div className="flex justify-start lg:w-0 lg:flex-1">
-                        <a href="#">
+                        <Link to="/">
                             <span className="sr-only">Workflow</span>
                             <img
                                 className="h-8 w-auto sm:h-10"
                                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                                 alt=""
                             />
-                        </a>
+                        </Link>
                     </div>
                     <div className="-mr-2 -my-2 md:hidden">
                         <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -110,7 +111,7 @@ export default function Header() {
                                             'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                                         )}
                                     >
-                                        <span>Solutions</span>
+                                        <span>Services</span>
                                         <ChevronDownIcon
                                             className={classNames(
                                                 open ? 'text-gray-600' : 'text-gray-400',
@@ -167,10 +168,10 @@ export default function Header() {
                         </Popover>
 
                         <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                            Pricing
+                            About Us
                         </a>
                         <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
-                            Docs
+                            Work
                         </a>
 
                         <Popover className="relative">
@@ -182,7 +183,7 @@ export default function Header() {
                                             'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                                         )}
                                     >
-                                        <span>More</span>
+                                        <span>Resources</span>
                                         <ChevronDownIcon
                                             className={classNames(
                                                 open ? 'text-gray-600' : 'text-gray-400',
@@ -246,15 +247,12 @@ export default function Header() {
                         </Popover>
                     </Popover.Group>
                     <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                        <a href="#" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-                            Sign in
-                        </a>
-                        <a
-                            href="#"
+                        <Link
+                            to="/contact"
                             className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                         >
-                            Sign up
-                        </a>
+                            Contact Us
+                        </Link>
                     </div>
                 </div>
             </div>
